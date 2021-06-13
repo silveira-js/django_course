125fcb07392343a1d2f72da1bbc505083945a25e
silveira-js <josuta@gmail.com>
2021-06-13 19:15:19 -0300

    adding configured views and urls

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path("january", views.january),
+    # path("february", views.february),
+    # path("march", views.march),
+
+    #the order matters
+    path("", views.index),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
+]
+
+
